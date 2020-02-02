cb444dbdd8cb4301ec3d6cad81bb353111e4c862
Ramsey Chapin <ramsey@ramseychapin.com>
2020-02-02 00:28:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,6 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find {|i| i.name == item_name}
-
       resp.write item.price
 
     else
